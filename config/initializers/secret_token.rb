da87bdc592b6ed2c0658a653f8886d380d97b320
Johannes Rummelhoff <j.von.rummel@gmail.com>
2012-04-24 17:49:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassRefinement::Application.config.secret_token = '3a9da681798d24024094ec0437578d9e2380b8512ec68a7ffe4a51b98bc167cf0d16086ec89b0b2d307ea6d655f99f7c6ba66a32e812a7bca94cab020fd56a20'
